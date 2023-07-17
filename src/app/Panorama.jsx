c976e3b014f4302e42205dd1f42a7cde55a3858d
Huzaifa0888 <115368837+Huzaifa0888@users.noreply.github.com>
2023-07-17 17:32:59 +0500

    done

--- src/app/Panorama.jsx
+++ src/app/Panorama.jsx
@@ -50,7 +50,7 @@ export const Panorama = ({}) => {
 
     hotspot1.position.set(8300.0, 500.0, 5000.0);
     hotspot2.position.set(10000.0, -500.0, 10.0);
-    hotspot3.position.set(10000.0, -500.0, 10.0);
+    hotspot3.position.set(10000.0, 500.0, 10.0);
     hotspot4.position.set(10000.0, -500.0, 10.0);
     // popupHotspot1.position.set(1800.0, 500.0, 8000.0);
     // popupHotspot2.position.set(1000.0, 500.0, 8000.0);
